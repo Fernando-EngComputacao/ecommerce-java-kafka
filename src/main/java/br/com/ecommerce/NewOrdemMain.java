965b8aa365aaaabcdf181201f97b44eeb7042c28
Fernando Furtado <fernando.furtadocarrilho@gmail.com>
2023-10-18 15:16:59 -0300

    first commit

---
+++ src/main/java/br/com/ecommerce/NewOrdemMain.java
@@ -0,0 +1,9 @@
+package br.com.ecommerce;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+public class NewOrdemMain {
+    public static void main (String[] args) {
+        KafkaProducer<String, String>
+    }
+}
