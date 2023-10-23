257e1f0da6144e368b8ef820aaf1f65f379118c9
Fernando Furtado <fernando.furtadocarrilho@gmail.com>
2023-10-23 14:11:27 -0300

    Producers, Consumers and Logger

--- src/main/java/br/com/ecommerce/producer/NewOrdemMain.java
+++ src/main/java/br/com/ecommerce/producer/NewOrdemMain.java
@@ -1,5 +1,6 @@
 package br.com.ecommerce.producer;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -12,14 +13,20 @@ public class NewOrdemMain {
     public static void main (String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "124,123,125";
+        var email = "Welcome! We're processing your email address";
         var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDEM", value, value);
-        producer.send(record, (data, ex) -> {
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL",email,email);
+
+         Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println(data.topic() + ":::partition" + data.partition() + "/offeset " + data.offset() + "/timestamp " + data.timestamp());
-        }).get();
+        };
+
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties () {
